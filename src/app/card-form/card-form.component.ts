0faf690f214a8f83d6c23f69ba20a8a6cd9fc56a
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-18 21:59:53 +0200

    implement validator to check user input

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './card-form.component.html',
-  styleUrl: './card-form.component.css'
+  styleUrl: './card-form.component.css',
+  
 })
 
 export class CardFormComponent {
 
   cardForm = new FormGroup({
-    name: new FormControl('')
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ])
   });
 
 }
