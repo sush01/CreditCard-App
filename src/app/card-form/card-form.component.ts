342d1985dc0dc7e5815c6619b5a4f554279b0f89
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-19 22:13:58 +0200

    create class name Dateformcontrol and handle submission

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -15,13 +15,26 @@ export class CardFormComponent {
 
   cardForm = new FormGroup({
     name: new FormControl('', [Validators.required,Validators.minLength(3)]),
-    cardNumber: new FormControl(''),
-    expiration: new FormControl(''),
-    securityCode: new FormControl('')
+    cardNumber: new FormControl('', 
+      [Validators.required, 
+      Validators.minLength(16), 
+      Validators.maxLength(16)
+    ]),
+    expiration: new FormControl('', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]),
+    securityCode: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3)
+    ]
+    )
   });
 
   constructor(){
     
+  }
+
+  onSubmit(){
+    console.log('Form was submitted');
   }
 
 }
